2912b76e1558a5538d7aa6d3de608f19fa404a15
olivierba <olivierba@google.com>
2019-11-06 10:34:09 +0100

    variable for forseti module folder after init

--- script-pre.sh
+++ script-pre.sh
@@ -1,2 +1,9 @@
 
-./.terraform/modules/forseti-install-simple/helpers/setup.sh -p $projectid -o $orgid -e
+gcloud compute routers nats create nat-config \
+    --router=nat-router \
+    --auto-allocate-nat-external-ips \
+    --nat-all-subnet-ip-ranges \
+    --enable-logging
+
+
+./.terraform/modules/forseti-install-simple/$forsetimodulefolder/helpers/setup.sh -p $projectid -o $orgid -e
