c1565a4a3078e47c5b4038c5a32a96f12d572ff6
olivierba <olivierba@google.com>
2019-11-19 17:17:29 +0100

    control script

--- sample-deployment/test_deploy.sh
+++ sample-deployment/test_deploy.sh
@@ -1,2 +1,5 @@
+rm tfplan.tfplan
+rm tfplan.json
 terraform plan -out=tfplan.tfplan
-./terraform-validator-linux-amd64 validate tfplan.tfplan --policy-path=~/policy-library
+terraform show -json tfplan.tfplan > tfplan.json
+./terraform-validator-linux-amd64 validate tfplan.json --policy-path=/home/olivier/policy-library --project orbital-signal-243013
